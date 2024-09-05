1ed43d7c8873c6894cb76e1b013140146cd5a68c
BrunoZambuze <zambuzesilva@hotmail.com>
2024-09-05 07:51:41 -0300

    FinalizacaoDaAtividade

---
+++ AtividadeHilton/src/PadraoProjetoState/MarioState.java
@@ -0,0 +1,10 @@
+package PadraoProjetoState;
+
+public interface MarioState {
+
+	public MarioState pegarCogumelo();
+	public MarioState pegarFlor();
+	public MarioState pegarPena();
+	public MarioState levarDano();
+	
+}
---
+++ AtividadeHilton/src/PadraoProjetoState/PrincipalMain.java
@@ -0,0 +1,18 @@
+package PadraoProjetoState;
+
+public class PrincipalMain {
+
+	public static void main(String[] args) {
+		Mario mario = new Mario();
+		mario.PegarCogumelo();
+		mario.pegarPena();
+		mario.levarDano();
+		mario.pegarFlor();
+		mario.pegarFlor();
+		mario.levarDano();
+		mario.pegarPena();
+		mario.levarDano();
+		mario.levarDano();
+	}
+	
+}
